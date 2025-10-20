98735affbe643aa1255c78fc0aaf579ca5198cd4
pponya11 <kah1722291@gmail.com>
2025-10-20 20:58:26 +0900

    feat: 기본 실행 메인 구조 및 빈 문자열/null 입력 시 0 반환

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class StringCalculator {
+    public static int calculate(String input){
+        if(input == null || input.trim().isEmpty()) return 0;
+
+        throw new UnsupportedOperationException("아직 구현 안 됨");
+    }
+}
