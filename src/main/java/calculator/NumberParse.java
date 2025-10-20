9bca16bf04d066afba6ff210035dc9343461c570
pponya11 <kah1722291@gmail.com>
2025-10-20 21:08:32 +0900

    feat: 문자열을 숫자 리스트로 변환 후, 합산하는 로직 추가

---
+++ src/main/java/calculator/NumberParse.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberParse {
+    public static List<Integer> parse(String input){
+        String[] numbers= Delimiter.split(input);
+
+        try{
+            return Arrays.stream(numbers)
+                    .map(Integer::parseInt)
+                    .peek(NumberParse::validateNumber)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+        }
+    }
+
+    private static void validateNumber(int num){
+        if(num < 0) throw new IllegalArgumentException("음수값은 허용되지 않습니다");
+    }
+}
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import java.util.List;
+
 public class StringCalculator {
     public static int calculate(String input){
         if(input == null || input.trim().isEmpty()) return 0;
 
-        throw new UnsupportedOperationException("아직 구현 안 됨");
+        List<Integer> numbers= NumberParse.parse(input);
+        return numbers.stream().reduce(0, Integer::sum);
     }
 }
